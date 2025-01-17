d604aaa97269cbcd652b1376a3aa8f2e77f55378
Marwin2002 <musicbotid@gmail.com>
2025-01-17 11:17:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 from os import getenv
-
+import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
 
-API_ID = int(getenv("API_ID", "12380656"))
-API_HASH = getenv("API_HASH", "d927c13beaaf5110f25c505b7c071273")
+API_ID = os.getenv("API_ID", "12380656"))
+API_HASH = os.getenv("API_HASH", "d927c13beaaf5110f25c505b7c071273")
 
 BOT_TOKEN = getenv("BOT_TOKEN")
 MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://architect2002:architect2002@cluster0.ccinu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
