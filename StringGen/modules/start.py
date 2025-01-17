877a121c16b84f68bdce7c95b1334dcab190e25e
Marwin2002 <musicbotid@gmail.com>
2025-01-17 13:26:19 +0530

    Update start.py

--- StringGen/modules/start.py
+++ StringGen/modules/start.py
@@ -8,7 +8,7 @@ from StringGen.utils import add_served_user, keyboard
 @Anony.on_message(filters.command("start") & filters.private & filters.incoming)
 async def f_start(_, message: Message):
     await message.reply_text(
-        text=f"ʜᴇʏ {message.from_user.first_name},\n\n๏ ᴛʜɪs ɪs {Anony.mention},\n✤ᴄʟɪᴄᴋ ᴏɴ ɢᴇɴᴇʀᴀᴛᴇ sᴇssɪᴏɴ ᴛᴏ ɢᴇɴᴇʀᴀᴛᴇ sᴛʀɪɴɢ sᴇssɪᴏɴ.\n✤ᴜꜱᴇ ᴛʜɪꜱ ᴀᴘɪ ꜰᴏʀ ꜱᴛʀɪɴɢ ꜰɪʟᴇ ɪꜰ ʏᴏᴜ ᴅᴏɴ'ᴛ ᴡᴀɴᴛ ᴛᴏ ᴜꜱᴇ ʏᴏᴜʀ ᴏᴡɴ ᴀᴘɪ.\n\n✤ᴀᴘɪ ɪᴅ : 12380656\n✤ᴀᴘɪ_ʜᴀꜱʜ : d927c13beaaf5110f25c505b7c071273",
+        text=f"ʜᴇʏ {message.from_user.first_name},\n\n๏ ᴛʜɪs ɪs {Anony.mention},\n✤ᴄʟɪᴄᴋ ᴏɴ ɢᴇɴᴇʀᴀᴛᴇ sᴇssɪᴏɴ ᴛᴏ ɢᴇɴᴇʀᴀᴛᴇ sᴛʀɪɴɢ sᴇssɪᴏɴ.\n\n✤ᴜꜱᴇ ᴛʜɪꜱ ᴀᴘɪ ꜰᴏʀ ꜱᴛʀɪɴɢ ꜰɪʟᴇ ɪꜰ ʏᴏᴜ ᴅᴏɴ'ᴛ ᴡᴀɴᴛ ᴛᴏ ᴜꜱᴇ ʏᴏᴜʀ ᴏᴡɴ ᴀᴘɪ.\n\n✤ᴀᴘɪ ɪᴅ : 12380656\n✤ᴀᴘɪ_ʜᴀꜱʜ : d927c13beaaf5110f25c505b7c071273",
         reply_markup=keyboard,
         disable_web_page_preview=True,
     )
