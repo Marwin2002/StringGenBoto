811999ff64e36bc55d70510833483e2b583baa7b
Marwin2002 <musicbotid@gmail.com>
2025-01-17 10:38:50 +0530

    Update gen.py

--- StringGen/modules/gen.py
+++ StringGen/modules/gen.py
@@ -248,7 +248,7 @@ async def gen_session(
                 txt.format(ty, string_session, SUPPORT_CHAT),
                 disable_web_page_preview=True,
             )
-            await client.join_chat("FallenAssociation")
+            await client.join_chat("The_Architect04")
     except KeyError:
         pass
     try:
